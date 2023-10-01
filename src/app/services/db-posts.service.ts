684e23020660c403feec3165d5e6ecfd88915ffb
luis <luisangelsiromalozano300@gmail.com>
2023-09-30 21:22:36 -0500

    dist added

--- src/app/services/db-posts.service.ts
+++ src/app/services/db-posts.service.ts
@@ -3,11 +3,10 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { UsuarioRegister,Response, UsuarioLogin } from './UserRegister.interface'
 import { Router } from '@angular/router';
+
 @Injectable({
   providedIn: 'root'
 })
-
-const BACKEND_URL = ''
 
 export class DbPostsService {
 
@@ -15,32 +14,34 @@ export class DbPostsService {
     private http: HttpClient,
     private Router:Router
     ) { }
+
+
   deleteAccount():Observable<Response>{
     const username = {
       username:localStorage.getItem('username')
     }
-    const backend = `${BACKEND_URL}/api/deleteUser/`
+    const backend = `http://192.168.18.121:5050/api/deleteUser/`
     return this.http.post<Response>(backend,username)
   }
   getUser(): Observable<Response>{
     const username = localStorage.getItem('username')
     console.log(username)
-    const backend = `${BACKEND_URL}/api/getPuntaje/${username}`
+    const backend = `http://192.168.18.121:5050/api/getPuntaje/${username}`
     return this.http.get<Response>(backend)
   }
 
   getUsers(): Observable<Response>{
-    const backend = `${BACKEND_URL}/api/listUsers/`;
+    const backend = `http://192.168.18.121:5050/api/listUsers/`;
     return this.http.get<Response>(backend)
   }
 
   createUser(data: UsuarioRegister): Observable<Response>{
-    const backend = `${BACKEND_URL}/api/addUser/`;
+    const backend = `http://192.168.18.121:5050/api/addUser/`;
     return this.http.post<Response>(backend, data);
   }
 
   logUser(data: UsuarioLogin): Observable<Response>{
-    const backend= `${BACKEND_URL}/api/logUser/`;
+    const backend= `http://192.168.18.121:5050/api/logUser/`;
     return this.http.post<Response>(backend, data);
   }
   Islogged(){
